58676c913401eefc7524b373e829d79ae86917aa
Jose De Anda <josdandar@gmail.com>
2022-04-13 20:14:07 -0700

    Segundo commit

---
+++ Session-06/js/ejerciciostoday.js
@@ -0,0 +1,57 @@
+// Ejercio 1
+// -Pedir al usuarui un numero entre el 1 y el 10
+// -imprimir todos los numeros pares que existen entre 1 y el numero selccionado
+// Ejercicio 2
+// imprimir en consla la sumaa total de todos los digitos de una cantidad
+// input: 1234
+// output: 10
+// Ejercicio 3
+// mostar en consola el sigueinte patron
+// // *
+// // **
+// // ***
+// // ****
+// // *****
+// // ******
+// // *******
+
+
+// Ejercicio 1 
+
+// -Pedir al usuario un numero entre el 1 y el 10
+// -imprimir todos los numeros pares que existen entre 1 y el numero selccionado
+
+
+//Pedir un prompt al usuario del 1-10
+//Asignarlo a una variable 
+// Buscar manera para conocer los pares 
+//Ir numero por numero con la solucion 
+
+
+
+// let numeroEntrada = Number(prompt("Ingresa un numero del 1-10"))
+// function numeroPar (){
+
+//         for(let index= 0 ; index <= numeroEntrada; index++){
+//             if(index % 2 == 0){
+//                 console.log(index)
+//             }
+//         }
+
+// }
+// numeroPar ()
+
+
+//EJERCICIO 2 
+
+// imprimir en consla la sumaa total de todos los digitos de una cantidad
+// input: 1+2+3+4
+// output: 10
+
+function sumaDeDigitos(){
+    let digitos = Number(prompt("Escribe una cantidad superior a los dos digitos"));
+    digitosSeparados = digitos.split
+    console.log(digitosSeparados)
+
+}
+sumaDeDigitos()
--- Session-06/js/randomChar.js
+++ Session-06/js/randomChar.js
@@ -1,6 +1,6 @@
 
 let inputSize = prompt("Please enter an input size for the string")
-
+let randomCharacter
 function randomString(){
     
 }
